830ac26f2488d8512de15af2740db950112b70ca
Kraiovyi Denys <tonopik@gmail.com>
2023-06-11 14:30:32 +0300

    build home section in Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,26 @@
-import React from "react";
+import "./home.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section>
+      <div className="home flex ">
+        <div className="wrapper">
+          <div className="content">
+            <h1>Аграрний</h1>
+            <h2>інтернет-магазин</h2>
+            <p>
+              Основна сфера діяльності – дистрибуція насіння, засобів <br />{" "}
+              захисту рослин, мінеральних макро - та мікродобрив
+            </p>
+            <button>Про компанію</button>
+          </div>
+        </div>
+        <div className="main_image">
+          <img src="./images/dat-main.png" alt="man" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
